3c0f0761b09a631005e03e8e1f1d52bf27d90c6b
amolbokane05 <amolbokane05@gmail.com>
2022-11-10 11:42:25 +0530

    All code Added

--- Sample-04.java
+++ Sample-04.java
@@ -1,9 +1,20 @@
 public class Test{
 
+<<<<<<< Updated upstream
 public void add(){
 
     System.out.println("Adding Method");
 
+=======
+public static void main(String [] args){
+>>>>>>> Stashed changes
 
-}
+   int a= 10;
+   int b= 20;
+  int c = a+b;
+
+  System.out.println(c);
+
+     }
+   }
 
